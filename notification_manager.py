1af38c53ebb3032a8addda4efa1a9402d4e4a86c
Elżbieta Skrzypek <125986679+ElzbietaSkrzypek@users.noreply.github.com>
2023-04-19 16:24:16 +0200

    Add files via upload

---
+++ notification_manager.py
@@ -0,0 +1,47 @@
+from twilio.rest import Client
+import smtplib
+import requests
+
+TWILIO_SID = YOUR_TWILIO_ACCOUNT_SID
+TWILIO_AUTH_TOKEN = YOUR TWILIO AUTH TOKEN
+TWILIO_VIRTUAL_NUMBER = YOUR TWILIO VIRTUAL NUMBER
+TWILIO_VERIFIED_NUMBER = YOUR TWILIO VERIFIED NUMBER
+SHEET_USERS_API = "https://api.sheety.co/c2af5a9dc826689fcfa762efd3b07f64/flightDeals/users"
+OBJECT_ID = 1
+my_email = YOUR EMAIL
+email_password = YOUR EMAIL PASSWORD
+MAIL_PROVIDER_SMTP_ADDRESS = YOUR EMAIL PROVIDER SMTP ADDRESS #for gmail: "smtp.gmail.com"
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+###########################################################
+    def send_emails(self, message, google_flight_link):
+        response = requests.get(url=SHEET_USERS_API)
+        data = response.json()
+        # print(data)
+        users_emails = [user["email"] for user in data["users"]]
+
+        for user in users_emails:
+            with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=email_password)
+                connection.sendmail(from_addr=my_email,
+                                    to_addrs=user,
+                                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode(
+                                        'utf-8')
+                                    )
+
+
